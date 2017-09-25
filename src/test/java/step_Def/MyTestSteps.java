ad26ded2a5932762a84ddf81bb88c949d3870535
dell <sonia.gupta@franciscanecare.net>
2017-09-25 13:09:40 +0530

    committed on sep 25, 17

--- src/test/java/step_Def/MyTestSteps.java
+++ src/test/java/step_Def/MyTestSteps.java
@@ -133,6 +133,7 @@ public WebDriver dr;
         //System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver_win32\\chromedriver.exe");
 	   //System.setProperty("webdriver.chrome.driver", "D:\\selenium\\chromedriver.exe");
+		//
 		dr= new ChromeDriver();
 		dr.manage().window().maximize();
 		scenario= s.getSourceTagNames();
